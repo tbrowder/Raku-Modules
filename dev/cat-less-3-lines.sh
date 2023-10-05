a5ca8e87c5c6c818c363eb3b731e4544c7832853
Tom Browder <tbrowder@acm.org>
2023-10-05 08:01:28 -0500

    add adjective

--- dev/cat-less-3-lines.sh
+++ dev/cat-less-3-lines.sh
@@ -1,8 +1,10 @@
 # antononcube
+# shortest
 echo "antononcube: 8 chars"
 raku -e 'say $_ for "somefile".IO.lines[^(*-3)]'
 
 # lizmat
+# prettiest
 echo "lizmat: 10 chars:"
 raku -e 'say $_ for "somefile".IO.lines.head(*-3)'
 
